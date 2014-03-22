5b0b98f1d736b0f9c4d553ed2ee6d7e134f6b28b
invisumer <kristian.pacquing@gmail.com>
2014-03-22 18:54:25 +0100

    ex4 rev3

--- 2014-03-21/python/exercise4.py
+++ 2014-03-21/python/exercise4.py
@@ -4,7 +4,7 @@ l = 0.30;
 h = 0.20;
 unit = MKPOL([[[0,0],[0,0.2+h],[l,h],[l,0.2+h]],[[1,2,3,4]],None])
 unit = MAP([S1,S3,S2])(PROD([unit,Q(0.80)]))
-ramp = T([1,2,3])([7.44,-0.80,-4.0])(STRUCT(NN(19)([unit,T([1,3])([l,h])])))
+ramp = T([1,2,3])([7.10,-0.80,-4.0])(STRUCT(NN(19)([unit,T([1,3])([l,h])])))
 
 
 building = STRUCT([solid_model_3D,ramp])
