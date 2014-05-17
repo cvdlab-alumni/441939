8c7d0cbe854a06ffee91398c8742c5f3cb0e582b
invisumer <kristian.pacquing@gmail.com>
2014-05-17 18:03:50 +0200

    corretto commento

--- 2014-05-16/python/exercise3.py
+++ 2014-05-16/python/exercise3.py
@@ -4,7 +4,7 @@ def multipleDiagram2cell(diagram,master,toRemove,toMerges):
 	V,CV = diagram
 	# Remove specified cells in toRemove from diagram first
 	diagram = V,[cell for k,cell in enumerate(CV) if not (k in toRemove)]
-	# Insert diagram in all the cells indicated in toMerges
+	# Insert diagram all the cells indicated in toMerges
 	toMerges = list(sort(toMerges)) # Sorting is required to avoid random renumbering of cells
 	for i in xrange(len(toMerges)):
 		master = diagram2cell(diagram,master,toMerges[i]-i)
