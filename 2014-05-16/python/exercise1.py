7cb7c3363a6d47f71d558874ef3b1c738b620c92
invisumer <kristian.pacquing@gmail.com>
2014-05-24 13:46:11 +0200

    more details

--- 2014-05-16/python/exercise1.py
+++ 2014-05-16/python/exercise1.py
@@ -16,11 +16,18 @@ door1_h = 2.40
 door2_h = 2.10
 door1_w = 1.20
 door2_w = 0.80
+door1_f = 0.10
+door2_f = door1_f
+door1_th = 0.15
+door2_th = 0.02
 
 # Window measures
 window_h1 = 0.95
 window_h2 = 0.95 + 1.80
+window_h = window_h2-window_h1
 window_w = 1.0
+window_f = 0.05
+window_th = 0.05
 
 ''' FUNCTIONS '''
 
@@ -36,16 +43,38 @@ def removeCells(toRemove, diagram):
 	V,CV = diagram
 	return V,[cell for k,cell in enumerate(CV) if not (k in toRemove)]
 
-def door(w,h,p,h_floor,axis='x'):
+def door_hole(w,h,p,h_floor,axis='x'):
 	shape = [1,1,2]
 	size = [[w],[p],[h,h_floor-h]] if axis=='x' else [[p],[w],[h,h_floor-h]]
 	return assemblyDiagramInit(shape)(size)
 
-def window(w,h1,h2,p,h_floor,axis='x'):
+def window_hole(w,h1,h2,p,h_floor,axis='x'):
 	shape = [1,1,3]
 	z_size = [h1,h2-h1,h_floor-h2]
 	size = [[w],[p],z_size] if axis=='x' else [[p],[w],z_size]
 	return assemblyDiagramInit(shape)(size)
+
+def door(w,h,p,frame,th,axis='x',dtype='main'):
+	x_shape = 6 if dtype=='main' else 3
+	y_shape = 3
+	shape = [x_shape,y_shape,2] if axis=='x' else [y_shape,x_shape,2]
+	shutter = (w-frame*3.0)/2 if dtype=='main' else (w-frame*2)
+	x_size = [frame,shutter,frame/2.0,frame/2.0,shutter,frame] if dtype=='main' else [frame,shutter,frame]
+	y_size = [(p-th)/2.0,th,(p-th)/2.0]
+	z_size = [h-frame,frame]
+	size = [x_size,y_size,z_size] if axis=='x' else [y_size,x_size,z_size]
+	door = assemblyDiagramInit(shape)(size)
+	return door
+
+def window(w,h,p,frame,th,axis='x'):
+	shape = [6,3,3] if axis=='x' else [3,6,3]
+	glass = (w-frame*4)/2.0
+	x_size = [frame,glass,frame,frame,glass,frame]
+	y_size = [(p-th)/2.0,th,(p-th)/2.0]
+	z_size = [frame,h-frame*2,frame]
+	size = [x_size,y_size,z_size] if axis=='x' else [y_size,x_size,z_size]
+	window = assemblyDiagramInit(shape)(size)
+	return window
 
 ''' WALLS '''
 
@@ -66,15 +95,15 @@ master = diagram2cell(south,master,13)
 north = assemblyDiagramInit([5,3,1])([[1.5,in_wall,4,in_wall,1.5],[1,in_wall,3],[h]])
 master = diagram2cell(north,master,16)
 
-''' DOORS AND WINDOWS '''
+''' DOORS AND WINDOWS HOLES'''
 
-# Creating all type of doors and windows
+# Creating all type of doors and windows holes
 
-entrance = door(door1_w,door1_h,out2_wall,h,'y')
-door_x = door(door2_w,door2_h,in_wall,h,'x')
-door_y = door(door2_w,door2_h,in_wall,h,'y')
-window_x = window(window_w,window_h1,window_h2,out1_wall,h,'x')
-window_y = window(window_w-0.20,window_h1,window_h2,out1_wall,h,'y')
+entrance = door_hole(door1_w,door1_h,out2_wall,h,'y')
+door_x = door_hole(door2_w,door2_h,in_wall,h,'x')
+door_y = door_hole(door2_w,door2_h,in_wall,h,'y')
+window_x = window_hole(window_w,window_h1,window_h2,out1_wall,h,'x')
+window_y = window_hole(window_w-0.20,window_h1,window_h2,out1_wall,h,'y')
 
 # Doors inception
 
@@ -126,12 +155,45 @@ master = diagram2cell(south_windows,master,10)
 master = diagram2cell(north_windows,master,14)
 master = diagram2cell(bath_windows,master,21)
 
+''' DOOR AND WINDOW '''
+
+# Door and windows diagrams
+
+entrance = door(door1_w,door1_h,out2_wall,door1_f,door1_th,'y')
+door_x = door(door2_w,door2_h,in_wall,door2_f,door2_th,'x','normal')
+door_y = door(door2_w,door2_h,in_wall,door2_f,door2_th,'y','normal')
+window_x = window(window_w,window_h,out1_wall,window_f,window_th,'x')
+window_y = window(window_w,window_h,out1_wall,window_f,window_th,'y')
+
+# Door and window inceptions
+
+master = diagram2cell(entrance,master,41)
+master = diagram2cell(door_y,master,53)
+master = diagram2cell(door_x,master,47)
+master = diagram2cell(door_x,master,48)
+master = diagram2cell(door_y,master,54)
+
+master = diagram2cell(window_x,master,73)
+master = diagram2cell(window_x,master,75)
+master = diagram2cell(window_x,master,61)
+master = diagram2cell(window_x,master,63)
+master = diagram2cell(window_y,master,78)
+
 ''' REMOVING CELLS '''
 
-toRemove = [28,27,26,29,31] + [36,37,38,33,25,23] + [41,54,48,50,58] + [78,81,87,66,69]
+# VIEW(addCellNumbers(master, RED, 0.1))
+toRemove = [23,25,36,37,38,31,29,26,27,28,33,
+	105,107,109,111,81,83,85,87,
+	117,129,139,143,157,161,171,183,
+	203,212,221,230,200,227,197,206,215,224,
+	257,266,275,284,254,281,251,260,269,278,
+	452,449,446,443,434,425,416,413,410,407,
+	311,320,329,338,308,335,305,314,323,332,
+	365,374,383,392,362,389,359,368,377,386
+	]
 master = removeCells(toRemove,master)
 
 ''' VIEWS '''
 
-VIEW(draw(master))
-VIEW(addCellNumbers(master))
+# VIEW(draw(master))
+# VIEW(addCellNumbers(master))
--- 2014-05-16/python/exercise2.py
+++ 2014-05-16/python/exercise2.py
@@ -26,17 +26,17 @@ def makeStruct(struct):
 	return STRUCT(CAT(AA(MKPOLS)(evalStruct(struct))))
 
 def step(w,p):
-	controls = [[0.0,0.0],[0.0,-p],[w/2.0,-p],[w,-p],[w,0.0]]
+	controls = [[0.0,0.0],[0.0,-p],[w/2.0,-p*7.0/5.0],[w,-p],[w,0.0]]
 	knots = [0,0,0,1,2,3,3,3]
 	spline = BSPLINE(2)(knots)(controls)
-	V,FV = larMap(spline)(larDom(knots))
-	return V, [range(len(V))]
+	V,EV = larMap(spline)(larDom(knots))
+	return V,[range(len(EV))]
 
 # With larcc
 def curved_ramp(w1,w2,p1,p2,h,N):
 	h_step = h/(N+0.0)
-	p_step = (p1-p2)/(N+0.0)
-	w_step = (w1-w2)/2.0/(N+0.0)
+	p_step = (p1-p2)/(N-1.0) if N>1 else (p1-p2)
+	w_step = (w1-w2)/2.0/(N-1.0) if N>1 else (w1-w2)/2.0
 
 	w,p = w1, p1
 	assembly = []
@@ -55,7 +55,18 @@ def rotationalSurface(profile):
       return [COS(v)*x,SIN(v)*x,z]
    return rotationalSurface0
 
-def hill(R,h):
+def hill():
+	bezier1 = larBezierCurve([[0,0,0],[2,0,0],[6,0,0],[8,0,0]])
+	bezier2 = larBezierCurve([[0,3,0],[1.5,3,1.5],[6,3,5.0],[8,3,0]])
+	bezier3 = larBezierCurve([[0,4,0],[2,4,-2],[6,4,-3],[8,4,0]])
+	bezier4 = larBezierCurve([[0,5,0],[3,5,7.0],[6,5,2.0],[8,5,0]])
+	bezier5 = larBezierCurve([[0,10,0],[2,10,0],[6,10,0],[8,10,0]])
+
+	surface = BEZIER(S2)([bezier1,bezier2,bezier3,bezier4,bezier5])
+	hill = larMap(surface)(larDomain([48,48]))
+	return hill
+
+def rounded_hill(R,h):
 	controls = [[R,0.0,0.0],[4*R/5.0,0.0,0.0],[R/2.0,0.0,h],[0.0,0.0,h]]
 	profile = larBezierCurve(controls)
 	surface = rotationalSurface(profile)
@@ -100,18 +111,21 @@ master = diagram2cell(apartmentB,master,1)
 ''' LANDING '''
 
 # Window
-window = window(window_w,window_h1,window_h2,out1_wall,h)
+window = window_hole(window_w,window_h1,window_h2,out1_wall,h,'x')
 landing_window = assemblyDiagramInit([3,1,1])([
 	[0.5,window_w,0.5],[out1_wall],[h]])
 landing_window = diagram2cell(window,landing_window,1)
-landing_window = removeCells([3], landing_window)
+landing_window = diagram2cell(window_x,landing_window,3)
+# VIEW(addCellNumbers(landing_window,CYAN,0.2))
 
 # Main door
-main_door = door(door_w,door_h,out2_wall,h)
+main_door = door_hole(door_w,door_h,out2_wall,h)
 landing_door = assemblyDiagramInit([3,1,1])([
 	[0.2,door_w,0.2],[out1_wall],[h]])
 landing_door = diagram2cell(main_door,landing_door,1)
-landing_door = removeCells([2], landing_door)
+main_door = door(door1_w,door1_h,out2_wall,door1_f,door1_th,'x')
+landing_door = diagram2cell(main_door,landing_door,2)
+# VIEW(addCellNumbers(landing_door,CYAN,0.3))
 
 # Landing type 1
 landing = assemblyDiagramInit([1,5,2])([
@@ -120,7 +134,9 @@ inter_landing =  assemblyDiagramInit([1,1,3])([
 	[2.0],[1.80],[1.55,fl_h,1.55]])
 landing = diagram2cell(landing_window,landing,9)
 landing = diagram2cell(inter_landing,landing,3)
-landing = removeCells([14,12,4,6,2,3], landing)
+# VIEW(addCellNumbers(landing,CYAN,0.3))
+landing = removeCells([68,66,4,2,3,6,28,37,46,55,25,52,22,31,40,49],landing)
+# VIEW(draw(landing))
 
 # Top landing
 top_landing = assemblyDiagramInit([1,5,2])([
@@ -130,14 +146,20 @@ top_landing = diagram2cell(landing_window,top_landing,8)
 top_hole = assemblyDiagramInit([2,2,1])([
 	[1.0,1.0],[2.0,2.0],[fl_h]])
 top_landing = diagram2cell(top_hole,top_landing,3)
-top_landing = removeCells([2,3,5,15,18,17,1], top_landing)
+# VIEW(addCellNumbers(top_landing,CYAN,0.2))
+top_landing = removeCells([1,2,3,126,123,125,5,
+	85,94,103,112,82,109,79,88,97,106,
+	27,36,45,54,24,51,21,30,39,48],top_landing)
+# VIEW(draw(top_landing))
 
 # Ground landing (with main door)
 ground_landing = assemblyDiagramInit([1,5,2])([
 	[2.0],[out1_wall,1.8,3.5,1.8,out1_wall],[fl_h,h]])
 ground_landing = diagram2cell(landing_door,ground_landing,9)
 ground_landing = diagram2cell(inter_landing,ground_landing,3)
-ground_landing = removeCells([13,4,6], ground_landing)
+# VIEW(addCellNumbers(ground_landing,CYAN,0.3))
+ground_landing = removeCells([49,4,6,21,27,33,39,17,23,29,35],ground_landing)
+# VIEW(draw(ground_landing))
 
 master = diagram2cell(ground_landing,master,3)
 master = diagram2cell(landing,master,3)
@@ -148,7 +170,8 @@ master = diagram2cell(top_landing,master,3)
 
 external_ramp = curved_ramp(4,2,2,1,1+fl_h,8)
 land = CUBOID([40,40,-0.5])
-hill = hill(5,2.5)
+hill = rounded_hill(5,2.5)
+# hill = hill()
 tree = TREE1(0.10,3,25,1.2,16,16)
 trees = STRUCT(NN(4)([T(1)(4),tree]))
 sidewalk = CUBOID([2.0,-20,0.1625])
@@ -195,10 +218,11 @@ landing_block = larApply(t(7.7,7.6,0))(landing_block)
 
 # Ramps in PYPLASM
 # ramp(w,l,h,n,raiser=0.2):
-ramp = ramp(1.0,0.4,0.20555556,9,0.3)
+ramp_h, ramp_r = 0.13214285714286, 0.16785714285714
+ramp = ramp(1.0,0.25,ramp_h,15,ramp_r)
 ramp1 = T([1,2])([1.0+7.7,1.8+0.25])(R([1,2])(PI/2)(ramp))
 ramps1 = STRUCT([ramp1]+[T(3)(3.7),ramp1]*2)
-ramp2 = T([1,2,3])([1.0+7.7,1.8*2+1.85+0.1,1.55+0.3])(R([1,2])(-PI/2)(ramp))
+ramp2 = T([1,2,3])([1.0+7.7,0.25+1.8+3.5,1.55+0.3])(R([1,2])(-PI/2)(ramp))
 ramps2 = STRUCT([ramp2]+[T(3)(3.7),ramp2]*2)
 ramps = [ramps1,ramps2]
 # VIEW(STRUCT(ramps + [draw(landing_block)]))
@@ -223,10 +247,12 @@ building = STRUCT([
 	draw(master),
 	COLOR(light_blue)(ramps),
 	COLOR(light_gray)(external_ramp),
-	COLOR(grass_green)(land),
-	COLOR(grass_green)(hill),
+	TEXTURE('textures/grass.jpg')(land),
+	TEXTURE('textures/grass.jpg')(hill),
+	# COLOR(grass_green)(land),
+	# COLOR(grass_green)(hill),
 	trees1, trees2, tree,sidewalk,
 	COLOR(light_red)(top)
 	])
-VIEW(building)
+# VIEW(building)
 VIEW(addCellNumbers(master,RED))
